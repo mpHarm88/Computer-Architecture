df8c62816d1f2f56f16c2077b9efa606c2fa4274
mpHarm88 <mpHarm88@users.noreply.github.com>
2020-10-08 15:16:20 -0700

    add

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -17,6 +17,7 @@ class CPU:
         self.ram = [0]*256
         self.reg = [0]*8
         self.pc = 0
+        self.inst_reg = list()
 
         # Initialize stack pointer
         self.reg[7] = 0xF4
@@ -88,93 +89,103 @@ class CPU:
 
         print()
 
+    def use_LDI(self):
+        # register address location
+        opperand_a = int(self.ram[self.pc+1], 2)
+
+        # value to be saved
+        opperand_b = int(self.ram[self.pc+2], 2)
+
+        # Assign value to address in register
+        self.reg[opperand_a] = opperand_b
+
+        # store result in instruction register
+        self.inst_reg.append(opperand_b)
+
+        # Increment pc by 3
+        self.pc+=3
+
+    def use_PRN(self):
+        # Store idx location
+        reg_idx = int(self.ram[self.pc+1], 2)
+
+        #print the value
+        print(self.reg[reg_idx])
+
+        # append the result to instruction register
+        self.inst_reg.append(self.reg[reg_idx])
+
+        # Increment pc by 2
+        self.pc+=2
+    
+    def use_MUL(self):
+        # Find index location of numbers to be multiplied
+        reg1 = int(self.ram[self.pc+1],2)
+        reg2 = int(self.ram[self.pc+2],2)
+
+        # Passinto alu for processing
+        self.alu("MUL", reg1, reg2)
+
+        # Increment pc by 3
+        self.pc+=3
+
+    def use_PUSH(self):
+        # decrement the stakc pointer
+        self.reg[7] -= 1
+
+        # find index and save vale into the register
+        reg_idx = int(self.ram[self.pc +1],2)
+        value = self.reg[reg_idx]
+
+        # Copy value to the correct address
+        SP = self.reg[7]
+        self.ram[SP] = value
+
+        self.pc+=2
+
+    def use_POP(self):
+        # stack ponter location
+        SP = self.reg[7]
+
+        # find value of SP in ram
+        value = self.ram[SP]
+
+        # find the index value where the vale should be saved
+        reg_idx = int(self.ram[self.pc+1], 2)
+
+        # Save value to the register
+        self.reg[reg_idx] = value
+
+        # Increment the stack index
+        self.reg[7] +=1
+
+        # Increment pc
+        self.pc+=2
+
     def run(self):
         """Run the CPU."""
-
-        # Instruction Register
-        inst_reg = list()
 
         while self.ram[self.pc] != 0:
 
             # If ram_read returns the LDI command then save the results to the appropriate register
             if int(self.ram_read(self.pc), 2) == LDI:
-
-                # register address location
-                opperand_a = int(self.ram[self.pc+1], 2)
-
-                # value to be saved
-                opperand_b = int(self.ram[self.pc+2], 2)
-
-                # Assign value to address in register
-                self.reg[opperand_a] = opperand_b
-
-                # store result in instruction register
-                inst_reg.append(opperand_b)
-
-                # Increment pc by 3
-                self.pc+=3
+                self.use_LDI()
             
             # print the value
             elif int(self.ram_read(self.pc),2) == PRN:
-                # Store idx location
-                reg_idx = int(self.ram[self.pc+1], 2)
-
-                #print the value
-                print(self.reg[reg_idx])
-
-                # append the result to instruction register
-                inst_reg.append(self.reg[reg_idx])
-
-                # Increment pc by 2
-                self.pc+=2
+                self.use_PRN()
             
             # Multiply 
             elif int(self.ram_read(self.pc), 2) == MUL:
-
-                # Find index location of numbers to be multiplied
-                reg1 = int(self.ram[self.pc+1],2)
-                reg2 = int(self.ram[self.pc+2],2)
-
-                # Passinto alu for processing
-                self.alu("MUL", reg1, reg2)
-
-                # Increment pc by 3
-                self.pc+=3
-            
+                self.use_MUL()
+           
+           # Push to stack
             elif int(self.ram_read(self.pc), 2) == PUSH:
-                
-                # decrement the stakc pointer
-                self.reg[7] -= 1
+                self.use_PUSH()
 
-                # find index and save vale into the register
-                reg_idx = int(self.ram[self.pc +1],2)
-                value = self.reg[reg_idx]
-
-                # Copy value to the correct address
-                SP = self.reg[7]
-                self.ram[SP] = value
-
-                self.pc+=2
-
+            # Pop off stack  
             elif int(self.ram_read(self.pc), 2) == POP:
-
-                # stack ponter location
-                SP = self.reg[7]
-
-                # find value of SP in ram
-                value = self.ram[SP]
-
-                # find the index value where the vale should be saved
-                reg_idx = int(self.ram[self.pc+1], 2)
-
-                # Save value to the register
-                self.reg[reg_idx] = value
-
-                # Increment the stack index
-                self.reg[7] +=1
-
-                # Increment pc
-                self.pc+=2
+                self.use_POP()
 
             # Exit the program if HLT byte code appears
             elif int(self.ram_read(self.pc),2) == HLT:
